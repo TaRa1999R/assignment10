806e7489ebc6213f8b2ad2347e479cd750402205
TaRa1999R <rezaietar78@gmail.com>
2023-08-12 21:42:54 +0330

    assignment10

--- 3.py
+++ 3.py
@@ -2,4 +2,40 @@
 
 class Date :
 
-    
+    def __init__ ( self , ruz , mah , sal ) :
+        #properties
+        self.day = ruz
+        self.month = mah
+        self.year = sal
+        self.joda_konande = "/"
+
+    #methods
+    def season ( self , date ) :                         #che mahi az sal
+        ...
+
+    def age ( self , bith_date , today ) :               #sen afrad
+        ...
+
+    def ruzaye_zendegi ( self , birth_date , today ) :   #ta emruz chand ruz az zendegit gozashte
+        ...
+
+    def week_day ( self , date ) :                       #che ruzi az hafte
+        ...
+
+    def count_down ( self , date ) :                     #chand ruz az sal baqi mande
+        ...
+
+    def reminder ( self , birth_date , today ) :         #chand ruz ta tavalod ya yek ruz khas baqi mande
+        ...
+
+    def symbol ( self , year ) :                         #namade un sal chie
+        ...
+
+    def kabise ( self , year ) :                         #aya sal kabise hast
+        ...
+
+    def miladi ( self , date ) :                         #tabdil shamsi b miladi
+        ...
+
+    def qamari ( self , date ) :                         #tabdil shamsi b qamari
+        ...
