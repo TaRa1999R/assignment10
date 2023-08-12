6741fbbcf8a64f27844c2dcfa644a9cfaf3d9bb9
TaRa1999R <rezaietar78@gmail.com>
2023-08-12 21:06:49 +0330

    assignment10

---
+++ 1.py
@@ -0,0 +1,27 @@
+print (" FRACTION ")
+
+class Fraction :
+
+    def __init__ ( self , up , down ) :
+        #properties
+        self.numerator = up
+        self.define_line = "/"
+        self.denominator = down
+
+    #methods
+    def add ( self , first , second ) :
+        ...
+
+    def subtraction ( self , first , second ) :
+        ...
+    
+    def multiplication ( self , first , second) :
+        ...
+    
+    def division ( self , first , second ) :
+        ...
+
+    def reverse ( self , fraction ) :
+        ...
+
+    
